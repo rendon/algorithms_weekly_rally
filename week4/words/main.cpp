a8d934de79d8c9ef940bf7da8f737ced1b356492
Rafael Rendon Pablo <rafaelrendonpablo@gmail.com>
2017-03-05 21:37:29 -0800

    Add solution for Words
    Problem: http://coj.uci.cu/24h/problem.xhtml?pid=1167

--- week4/words/main.cpp
+++ week4/words/main.cpp
@@ -1,81 +1,77 @@
-/* Copyright 2016 Rafael Rendón Pablo <rafaelrendonpablo@gmail.com> */
-// Problem: Words (http://coj.uci.cu/24h/problem.xhtml?pid=1167)
+/* Copyright 2017 Rafael Rendón Pablo <rafaelrendonpablo@gmail.com> */
 // region Template
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <queue>
+#include <map>
+
+#include <cstring>
+#include <cctype>
 using namespace std;
 typedef long long           int64;
 typedef unsigned long long  uint64;
-const double kEps   = 10e-8;
-const int kMax      = 1000;
-const int kInf      = 1 << 30;
+const int kMax      = (1 << 16) + 1;
+const int kUndefined = -1;
 // endregion
 
-struct PriorityNode {
-    int cost;
-    int node;
-    bool operator<(const PriorityNode& that) const {
-        return this->cost < that.cost;
-    }
+struct Node {
+    int first, last, length;
 };
 
-inline bool connected(const string& u, const string& v) {
-    return u[u.length()-1] == v[0];
+int dp[kMax][5];
+map<char, int> eq{{'A', 0}, {'E', 1}, {'I', 2}, {'O', 3}, {'U', 4}};
+
+int f(int active, int prevLast, const vector<Node>& nodes) {
+    // Base case: no nodes available
+    if (active == 0) {
+        return 0;
+    }
+    int& ans = dp[active][prevLast];
+    if (ans != kUndefined) {
+        return ans;
+    }
+
+    ans = 0;
+    int mask = (1 << nodes.size()) - 1;
+    for (int i = 0; i < int(nodes.size()); i++) {
+        int length = nodes[i].length;
+        if (nodes[i].first == prevLast && (active & (1 << i))) {
+            int newActive = active & (mask ^ (1 << i));
+            ans = std::max(ans, f(newActive, nodes[i].last, nodes) + length);
+        }
+    }
+    return ans;
 }
 
-int dijkstra(vector<string>& words, int root) {
-    int M = words.size();
-    vector<int> dist(M, -kInf);
-    dist[root] = words[root].length();
-    vector<bool> locked(M, false);
-    priority_queue<PriorityNode> Q;
-    Q.push({dist[root], root});
-    while (!Q.empty()) {
-        auto top = Q.top();
-        Q.pop();
-        if (locked[top.node]) {
-            continue;
-        }
-        locked[top.node] = true;
-        int u = top.node;
-        for (int v = 0; v < M; v++) {
-            if (v == u) {
-                continue;
-            }
-            if (!connected(words[u], words[v])) {
-                continue;
-            }
-            if (top.cost + int(words[v].length()) > dist[v]) {
-                dist[v] = top.cost + words[v].length();
-                Q.push({dist[v], v});
-            }
-        }
+int maximalComplexity(const vector<Node>& nodes) {
+    memset(dp, kUndefined, sizeof dp);
+    int M = nodes.size();
+    int best = 0;
+    for (int prevLast = 0; prevLast < 5; prevLast++) {
+        best = std::max(best, f((1 << M) - 1, prevLast, nodes));
     }
-    int maxLength = 0;
-    for (int d : dist) {
-        if (d != -kInf) {
-            maxLength = std::max(maxLength, d);
-        }
-    }
-    return maxLength;
+    return best;
 }
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int T;
     cin >> T;
     while (T--) {
         int M;
         cin >> M;
-        vector<string> words(M);
-        for (string& word : words) {
+        vector<Node> nodes(M);
+        for (int i = 0; i < M; i++) {
+            string word;
             cin >> word;
+            int length = word.length();
+            nodes[i] = {eq[word[0]], eq[word[length-1]], length};
         }
-        
-        int maxPathLength = 0;
-        for (int u = 0; u < M; u++) {
-            maxPathLength = std::max(maxPathLength, dijkstra(words, u));
-        }
-        cout << maxPathLength << endl;
+        cout << maximalComplexity(nodes) << endl;
     }
     return EXIT_SUCCESS;
 }
-
